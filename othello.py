b7f9bceb8caf109ae966cc456f0606040b7dabb8
David Vaughan <DavidRayVaughan00@gmail.com>
2015-12-01 11:41:33 -0800

    Create makeMove function and playerWins function stub

--- othello.py
+++ othello.py
@@ -1,5 +1,17 @@
 print "~Othello~"
 
-coordList = raw_input("Enter coordinate for move (row col): ").split()
-coord = {'i': int(coordList[0]), 'j': int(coordList[1])}
-print "Player chose coordinate (%d, %d)" %(coord['i'], coord['j'])
+def makeMove(player):
+    coordList = raw_input("Enter coordinate for move (row col): ").split()
+    coord = {'i': int(coordList[0]), 'j': int(coordList[1])}
+    print "Player %d chose (%d, %d)" %(player, coord['i'], coord['j'])
+
+    # Modify the board here
+
+    if playerWins(player):
+        print "Player %d wins!" %(player)
+
+def playerWins(player):
+    # stub
+    return False
+
+makeMove(0)
