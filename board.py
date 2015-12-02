4e13523e018205cfc9a2a13b423dbf603cf1f8cc
Aaron Whitehead <aaronbmwhitehead@gmail.com>
2015-12-01 16:26:43 -0800

    oh god david dont use spaces plz. Also, went through and addough and added a possible move check, and some other things, still need to handle the case in which both players cant make a move, but i have to head to class right now

--- board.py
+++ board.py
@@ -14,6 +14,8 @@ class board:
 		self.data[4][3] = 2
 		
 	def makeMove(self, x, y):
+		if (not self.check(x,y)):
+			return False
 		if self.data[x][y] == 0:
 			if self.currentMove:
 				self.data[x][y] = 1
@@ -111,9 +113,58 @@ class board:
 				print(self.data[x][y]),
 			print(" ")
 		print(" ")
-	
 		
+	def check(self, xSent, ySent):
+		storage = self.data[xSent][ySent]
+		temp = self.data
+		cursor = [xSent, ySent]
+		if self.currentMove:
+			temp[xSent][ySent] = 1
+		else:
+			temp[xSent][ySent] = 2
+		goal = -1
+		count = 0
+		for x in range(0,8):
+			cursor = [xSent, ySent]
+			goal = temp[cursor[0]][cursor[1]]
+			count = 0
 			
+			if goal == 1:
+				goal = 2
+			elif goal == 2:
+				goal = 1
+				
+			cursor = [xSent, ySent]
+			self.movePoint(cursor, x)
+			if goal == 0:
+				continue
+			
+			while (0 <= cursor[0] <= 7) and (0 <= cursor[1] <= 7) and temp[cursor[0]][cursor[1]] == goal:
+				count += 1
+				self.movePoint(cursor, x)
+			
+			
+			if (not (0 <= cursor[0] <= 7)) or (not (0 <= cursor[1] <= 7)) or temp[cursor[0]][cursor[1]] != temp[xSent, ySent]:
+				continue
+	
+			if count > 0:			
+				self.data[xSent][ySent] = storage
+				return True
+		self.data[xSent][ySent] = storage
+		return False
+		
+	def possible(self):
+		compare = -1
+		if self.currentMove:
+			compare = 1
+		else:
+			compare = 2		
+		for x in range(0,8):
+			for y in range(0,8):
+				if self.data[x][y] == 0 and self.check(x, y):
+					return True
+		return False
+	
 			
 			
 		
--- game.py
+++ game.py
@@ -1,48 +1,52 @@
 import board
+import random
 
 class Game:
-    def __init__(self, numPlayers):
-        self.playerIsHuman = [False, False]
-        for i in range(0, numPlayers):
-            self.playerIsHuman[i] = True
+	def __init__(self, numPlayers):
+		self.playerIsHuman = [False, False]
+		for i in range(0, numPlayers):
+			self.playerIsHuman[i] = True
 
-        self.board = board.board()
+		self.board = board.board()
 
-    def play(self):
-        player = 1
-        while self.board.done() != True:
-            self.board.write()
-            print "Player 0: %d -- Player 1: %d" %(self.board.count(True), self.board.count(False))
+	def play(self):
+		player = 1
+		while self.board.done() != True:
+			self.board.write()
+			print "Player 0: %d -- Player 1: %d" %(self.board.count(True), self.board.count(False))
+			player = 0 if (player == 1) else 1
 
-            player = 0 if (player == 1) else 1
+			if not self.board.possible():
+				print "No possible moves, skipping your turn"
+				continue
 
-            moveGood = False
-            while moveGood != True:
-                coord = {}
-                if self.playerIsHuman[player] == True:
-                    coord = self.getHumanCoord()
-                else:
-                    coord = self.getCompCoord()
-                moveGood = self.board.makeMove(coord['i'], coord['j'])
-                if moveGood != True:
-                    print "Invalid move."
+			moveGood = False
+			while moveGood != True:
+				coord = {}
+				if self.playerIsHuman[player] == True:
+					coord = self.getHumanCoord()
+				else:
+					coord = self.getCompCoord()
+				moveGood = self.board.makeMove(coord['i'], coord['j'])
+				if moveGood != True and self.playerIsHuman[player] == True:
+					print "Invalid move."
 
-            print "Player %d chose (%d, %d)" %(player, coord['i'], coord['j'])
-            self.board.update()
+			print "Player %d chose (%d, %d)" %(player, coord['i'], coord['j'])
+			self.board.update()
 
-        self.board.write()
-        print "Player 0: %d -- Player 1: %d" %(self.board.count(True), self.board.count(False))
-        print "Game over"
-        player0Score = self.board.count(True)
-        player1Score = self.board.count(False)
-        winner = 0 if (player0Score > player1Score) else 0
-        print "Player %d wins!" %(winner)
+		self.board.write()
+		print "Player 0: %d -- Player 1: %d" %(self.board.count(True), self.board.count(False))
+		print "Game over"
+		player0Score = self.board.count(True)
+		player1Score = self.board.count(False)
+		winner = 0 if (player0Score > player1Score) else 0
+		print "Player %d wins!" %(winner)
 
-    def getHumanCoord(self):
-        coordList = raw_input("Enter coordinate for move (row col): ").split()
-        coord = {'i': int(coordList[0]), 'j': int(coordList[1])}
-        return coord
+	def getHumanCoord(self):
+		coordList = raw_input("Enter coordinate for move (row col): ").split()
+		coord = {'i': int(coordList[0]), 'j': int(coordList[1])}
+		return coord
 
-    def getCompCoord(self):
-        # The AI's not very smart yet
-        return {'i': 0, 'j': 0}
+	def getCompCoord(self):
+		# The AI's not very smart yet
+		return {'i': random.randint(0,7), 'j': random.randint(0,7)}
